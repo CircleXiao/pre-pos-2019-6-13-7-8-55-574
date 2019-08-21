540bfd83c449f50fee6607fa66ecc0c098f6b37a
mmy <mmy_xize@163.com>
2019-08-21 16:11:53 +0800

    提交section2  practice2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,48 +1,22 @@
 'use strict';
 
-function countSameElements(collection) {
-  function find(arr, word) {
-    var ret = -1;
-    for (var i = 0; i < arr.length; i++) {
-      if (word === arr[i]) {
-        ret = i;
+function countSameElements(collection){
+  const resultList = [];
+  const result = new Map();
+
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i].length != 1) {
+      result.set(collection[i].charAt[0], collection[i].charAt[2]);
+    } else {
+      if (result.get(collection[i]) == null) {
+        result.set(collection[i], 1);
+      } else {
+        result.set(collection[i], result.get(collection[i]) + 1);
       }
-    }
-    return ret;
+    }   
   }
 
-  function search(arr, word, count) {
-    for (var i = 0; i < arr.length; i++) {
-      var position = find(word, arr[i][0]);
-      if (arr[i].length != 1) {
-        if (position === -1) {
-          word.push(arr[i][0]);
-          count.push(Number(arr[i][2]));
-        }
-        else {
-          count[position] += Number(arr[i][2]);
-        }
-      }
-      else {
-        if (position === -1) {
-          word.push(arr[i]);
-          count.push(1);
-        }
-        else {
-          count[position]++;
-        }
-      }
-    }
-  }
-
-  var words = [];
-
-  var num = [];
-
-  search(collection, words, num);
-  var answer = [];
-  for (var j = 0; j < words.length; j++) {
-    answer.push({key: words[j], count: num[j]});
-  }
-  return answer;
+  result.forEach(function (key, value) {
+    resultList.push({key: value, count: key});
+  })
 }
