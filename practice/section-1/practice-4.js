8ec3b92effb39760a540c7a18384707bc35a978f
mmy <mmy_xize@163.com>
2019-08-21 11:21:40 +0800

    提交practice3、4

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,34 +1,14 @@
 'use strict';
 
-function removeRepeat(collectionA) {
-  var r = [];
-  for (var i=0; i<collectionA.length; i++) {
-    var flag = true;
-    var temp = collectionA[i];
-    for (var j=0; j<r.length; j++) {
-      if(temp === r[j]) {
-        flag = false;
-        break;
-      }
-    }
-    if(flag) {
-      r.push(temp);
-    }
-  }
-  return r;
-}
-
 function collectSameElements(collectionA, collectionB) {
   var result = [];
-  var b = collectionB['value'];
-  for (var i=0; i<collectionA.length; i++) {
-    var temp = collectionA[i].key;
-    for (var j=0; j<b.length; j++) {
-      if (temp === b[j]) {
-        result.push(temp);
-        break;
-      }
-    }
+  var subCollectionB = collectionB['value'];
+  for (let i = 0; i < collectionA.length; i++) {
+    for (let j = 0; j < subCollectionB.length; j++) {
+      if (collectionA[i].key === subCollectionB[j]) {
+        result.push(collectionA[i].key);
+      }      
+    }    
   }
-  return removeRepeat(result);
+  return result;
 }
