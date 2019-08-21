5e718e5710d1b0229523b77c53cb12f0e7049ffb
mmy <mmy_xize@163.com>
2019-08-21 10:05:03 +0800

    提交priactice1

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,26 +1,40 @@
 'use strict';
 
-function removeRepeat(collectionA) {
-  var r = [];
-  for (var i=0; i<collectionA.length; i++) {
-    var flag = true;
-    var temp = collectionA[i];
-    for (var j=0; j<r.length; j++) {
-      if(temp === r[j]) {
-        flag = false;
-        break;
-      }
-    }
-    if(flag) {
-      r.push(temp);
-    }
-  }
-  return r;
-}
+// function removeRepeat(collectionA) {
+//   var r = [];
+//   for (var i=0; i<collectionA.length; i++) {
+//     var flag = true;
+//     var temp = collectionA[i];
+//     for (var j=0; j<r.length; j++) {
+//       if(temp === r[j]) {
+//         flag = false;
+//         break;
+//       }
+//     }
+//     if(flag) {
+//       r.push(temp);
+//     }
+//   }
+//   return r;
+// }
+
+// function collectSameElements(collectionA, collectionB) {
+//   var result = [];
+//   for (var i=0; i<collectionA.length; i++) {
+//     var temp = collectionA[i];
+//     for (var j=0; j<collectionB.length; j++) {
+//       if (temp === collectionB[j]) {
+//         result.push(temp);
+//         break;
+//       }
+//     }
+//   }
+//   return removeRepeat(result);
+// }
 
 function collectSameElements(collectionA, collectionB) {
   var result = [];
-  for (var i=0; i<collectionA.length; i++) {
+  for (var i=0; i <collectionA.length; i++) {
     var temp = collectionA[i];
     for (var j=0; j<collectionB.length; j++) {
       if (temp === collectionB[j]) {
@@ -29,5 +43,5 @@ function collectSameElements(collectionA, collectionB) {
       }
     }
   }
-  return removeRepeat(result);
+  return result;
 }
