622f4056b538f1905bffccd07c55fb4223826a50
mmy <mmy_xize@163.com>
2019-08-21 21:04:10 +0800

    提交section3 practice2

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,29 +1,14 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  var words = [];
-  var num = [];
-
-  function find(arrA, arrB, words, num) {
-    for (var i = 0; i < arrA.length; i++) {
-      for (var j = 0; j < arrB.value.length; j++) {
-        if (arrA[i].key === arrB.value[j]) {
-          words.push(arrA[i].key);
-          num.push(arrA[i].count - parseInt(arrA[i].count / 3));
-          break;
-        }
-      }
-      if (j === arrB.value.length) {
-        words.push(arrA[i].key);
-        num.push(arrA[i].count)
-      }
-    }
+  var subCollectionB = objectB.value;
+  for (let i = 0; i < subCollectionB.length; i++) {
+    for (let j = 0; j < collectionA.length; j++) {
+      if (subCollectionB[i] === collectionA[j].key) {
+        collectionA[j].count = collectionA[j].count - parseInt(collectionA[j].count / 3);
+        break;
+      }      
+    } 
   }
-
-  find(collectionA, objectB, words, num);
-  var answer = [];
-  for (var k = 0; k < words.length; k++) {
-    answer.push({key: words[k], count: num[k]});
-  }
-  return answer;
+  return collectionA;
 }
